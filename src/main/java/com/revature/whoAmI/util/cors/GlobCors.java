73fb0ff3465022ea2afb6d02524cdb5af09de9aa
trknox <love.lala2003@gmail.com>
2022-08-22 11:03:26 -0400

    changed cors link

--- src/main/java/com/revature/whoAmI/util/cors/GlobCors.java
+++ src/main/java/com/revature/whoAmI/util/cors/GlobCors.java
@@ -13,7 +13,7 @@ public class GlobCors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://who-am-i.azurewebsites.net/")//"http://localhost:4200/"
+                .allowedOrigins("*")//"http://localhost:4200/"
                 .allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS", "PATCH")
                 .allowedHeaders("header1", "header2", "header3","Authorization","content-type","access-control-allow-origin")
                 .exposedHeaders("header1", "header2", "Authorization", "content-type", "access-control-allow-origin");
