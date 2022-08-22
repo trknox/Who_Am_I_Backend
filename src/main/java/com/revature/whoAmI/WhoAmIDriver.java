177c8ba60f18552ba68e3c3516c8e5578cf337a4
trknox <love.lala2003@gmail.com>
2022-08-22 17:55:31 -0400

    test

--- src/main/java/com/revature/whoAmI/WhoAmIDriver.java
+++ src/main/java/com/revature/whoAmI/WhoAmIDriver.java
@@ -2,10 +2,15 @@ package com.revature.whoAmI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class WhoAmIDriver {
 
+    @GetMapping("/test")
+    public String message(){
+        return "IT'S WORKING!!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(WhoAmIDriver.class, args);
     }
